c51171375dd4de931bdf6b2974b034e16b2b00f6
davipeet8 <davidpeetp@gmail.com>
2020-08-19 22:25:00 -0400

    Improved syntax highlighting style

--- noteRenderer/src/app/app.module.ts
+++ noteRenderer/src/app/app.module.ts
@@ -14,6 +14,77 @@ import { ServicesModule } from './services/services.module';
 import { FetchRenderEventBusService } from '@services/fetch-render-event-bus.service';
 import { FileAccessAPIService } from '@services/file-access-api.service';
 
+import { MarkedOptions, MarkedRenderer } from 'ngx-markdown'
+
+export function markedOptionsFactory(): MarkedOptions 
+{
+  const renderer = new MarkedRenderer();
+
+  renderer.blockquote = (text:string) => 
+  {
+    return `<blockquote class="md-blockquote">${text}</blockquote>`;
+  }
+
+  renderer.heading = (text:string, level:number) => 
+  {
+    return `<h${level} class="md-h${level}">${text}</h${level}>`;
+  }
+
+  renderer.hr = () => 
+  {
+    return '<hr class="md-hr">';
+  }
+
+  renderer.list = (body:string, ordered:boolean, start:number) => 
+  {
+    let char = ordered ? 'o' : 'u'
+    return `<${char}l start="${start}" class="md-${char}l">${body}</${char}l>`;
+  }
+  
+  renderer.paragraph = (text:string) => 
+  {
+    return `<p class="md-p">${text}</p>`;
+  }
+
+  renderer.html = (html:string) => 
+  {
+    return `<section class="md-html">${html}</section>`;
+  }
+
+  renderer.listitem = (text:string) => 
+  {
+    return `<li class="md-li">${text}</li>`;
+  }
+
+  renderer.table = (header:string, body:string) => 
+  {
+    return `
+    <table class="md-table">
+      <thead>${header}</thead>
+      ${body}
+    </table>`;
+  }
+
+  renderer.codespan = (text:string) => 
+  {
+    return `<code class="md-codespan">${text}</code>`
+  }
+
+  renderer.strong = (text:string) => 
+  {
+    return `<strong class="md-strong">${text}</strong>`
+  }
+
+  return {
+    renderer: renderer,
+    gfm: true,
+    breaks: false,
+    pedantic: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +96,15 @@ import { FileAccessAPIService } from '@services/file-access-api.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MarkdownModule.forRoot({ loader: HttpClient, sanitize: SecurityContext.NONE }),
+    MarkdownModule.forRoot({ 
+      loader: HttpClient, 
+      sanitize: SecurityContext.NONE, 
+      markedOptions: 
+      {
+        provide: MarkedOptions, 
+        useFactory: markedOptionsFactory
+      }
+    }),
     ServicesModule,
   ],
   providers: [],
