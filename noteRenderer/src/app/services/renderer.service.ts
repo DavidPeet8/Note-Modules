78d8561e1ab00dad09436b6264676bc799bd5961
davipeet8 <davidpeetp@gmail.com>
2021-01-04 15:33:41 -0500

    Some angular config settings changed

---
+++ noteRenderer/src/app/services/renderer.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { katex } from 'katex';
+import { marked } from 'marked';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileAccessAPIService {
+
+}
