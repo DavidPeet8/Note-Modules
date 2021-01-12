bb710c003c55d68febeaea534b15b0cdf59e673e
davipeet8 <davidpeetp@gmail.com>
2021-01-11 19:59:35 -0500

    Updated file requests to only happen when tab is active

--- noteRenderer/src/app/services/file-access-api.service.ts
+++ noteRenderer/src/app/services/file-access-api.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-class File 
-{
+class File {
 	activeFileURI = "";
 	activeFileText = null;
 	lastModifiedTime = null;
 }
 
-class Dir 
-{
+class Dir {
 	dirlist: File[] = [];
 	lastModifiedTime = null;
 }
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class FileAccessAPIService {
 	rootDir = new Dir();
@@ -25,129 +23,118 @@ export class FileAccessAPIService {
 	publishCodeEvent = null;
 	host = 'http://localhost:8000'; // TODO: change this to being read from a file
 
-	constructor(private router: Router) 
-	{ 
+	constructor(private router: Router) {
 		this._fetchDirList();
 		this._fetchFile(this.activeFile.activeFileURI);
+		let checkDModified = this._checkDirListModified.bind(this);
+		let checkFModified = this._checkFileModified.bind(this);
 		setInterval(() => {
-			// This does not work as dir update time is not necessaily updated when time is updated
-			this._checkDirListModified.bind(this)(); 
-			//this._fetchDirList.bind(this)(); // This triggers a refresh every time this is called :()
-			this._checkFileModified.bind(this)();
+			if (!document.hidden) {
+				checkDModified();
+				checkFModified();
+			}
 		}, 5000);
 	}
 
-	async _fetchDirList()
-	{
+	async _fetchDirList() {
 		let dirlist;
 		await fetch(this.host + "/dirtree")
-		.then((data) => { 
-			return data.json();
-		})
-		.then((json) => {
-			console.log("Rerieved response for dirlist")
-			this.rootDir.dirlist = json.dirs;
-			this.rootDir.lastModifiedTime = json.modifyTime;
-			this.publishDirEvent();
-		})
-		.catch((err) => {
-			this.router.navigate(['install']);
-		});
+			.then((data) => {
+				return data.json();
+			})
+			.then((json) => {
+				console.log("Rerieved response for dirlist")
+				this.rootDir.dirlist = json.dirs;
+				this.rootDir.lastModifiedTime = json.modifyTime;
+				this.publishDirEvent();
+			})
+			.catch((err) => {
+				this.router.navigate(['install']);
+			});
 	}
 
-	async _fetchFile(fileName)
-	{
-		if (fileName == ""){ return; }
+	async _fetchFile(fileName) {
+		if (fileName == "") { return; }
 
 		await fetch(this.host + "/note" + fileName)
-		.then((data) => {
-			return data.json();
-		})
-		.then((json) => {
-			this.activeFile.activeFileText = json.fileData;
-			this.activeFile.lastModifiedTime = json.modifyTime;
-			this.publishCodeEvent(fileName);
-		})
-		.catch((e) => 
-		{
-			this._fetchDirList();
-		});
+			.then((data) => {
+				return data.json();
+			})
+			.then((json) => {
+				this.activeFile.activeFileText = json.fileData;
+				this.activeFile.lastModifiedTime = json.modifyTime;
+				this.publishCodeEvent(fileName);
+			})
+			.catch((e) => {
+				this._fetchDirList();
+			});
 	}
 
-	async _checkFileModified()
-	{
+	async _checkFileModified() {
 		if (this.activeFile.activeFileURI == "") { return; }
-		
+
 		await fetch(this.host + '/status/note' + this.activeFile.activeFileURI)
-		.then((data) => {
-			return data.text()
-		})
-		.then((text) => {
-			if (text != this.activeFile.lastModifiedTime) 
-			{
-				this._fetchFile(this.activeFile.activeFileURI); // File was Modified
-			}
-		})
-		.catch((e) => {
-			console.log("Failed to check for active file modifications", e)
-		});
+			.then((data) => {
+				return data.text()
+			})
+			.then((text) => {
+				if (text != this.activeFile.lastModifiedTime) {
+					this._fetchFile(this.activeFile.activeFileURI); // File was Modified
+				}
+			})
+			.catch((e) => {
+				console.log("Failed to check for active file modifications", e)
+			});
 	}
 
-	async _checkDirListModified()
-	{
+	async _checkDirListModified() {
 		await fetch(this.host + "/status/dirtree")
-		.then((data) => {
-			return data.text()
-		})
-		.then((text) => {
-			console.log("Checking Dir List - lastModifiedTime: " + text);
-			console.log("Old Last Modified Time: " + this.rootDir.lastModifiedTime);
-			if (text != this.rootDir.lastModifiedTime) 
-			{
-				console.log("different last modified time")
-				this._fetchDirList(); // DirTree was Modified
-			}
-		})
-		.catch((e) => {
-			console.log("Failed to check for Dir Tree modifications", e)
-		});
+			.then((data) => {
+				return data.text()
+			})
+			.then((text) => {
+				console.log("Checking Dir List - lastModifiedTime: " + text);
+				console.log("Old Last Modified Time: " + this.rootDir.lastModifiedTime);
+				if (text != this.rootDir.lastModifiedTime) {
+					console.log("different last modified time")
+					this._fetchDirList(); // DirTree was Modified
+				}
+			})
+			.catch((e) => {
+				console.log("Failed to check for Dir Tree modifications", e)
+			});
 	}
 
 	// ----------------------------PUBLIC INTERFACE -------------------------------
 
-	getDirList()
-	{
+	getDirList() {
 		return this.rootDir.dirlist;
 	}
 
-	getCurrentFile(): String 
-	{
+	getCurrentFile(): String {
 		return this.activeFile.activeFileText;
 	}
 
 	// Switch active file to partialURI and force an immediate fetch
-	switchActiveFile(partialURI): void
-	{
-		if(partialURI == this.activeFile.activeFileURI) return;
+	switchActiveFile(partialURI): void {
+		if (partialURI == this.activeFile.activeFileURI) return;
 		this.activeFile.activeFileURI = partialURI;
 		this._fetchFile(partialURI);
 	}
 
-	setPublishMethod(publishMethods): void
-	{
+	setPublishMethod(publishMethods): void {
 		this.publishDirEvent = publishMethods.dirEvent;
 		this.publishCodeEvent = publishMethods.codeEvent;
 	}
 
-	async doSearch(searchTerm, isDeep, callback)
-	{
+	async doSearch(searchTerm, isDeep, callback) {
 		let queryString = isDeep ? "?deep=true" : "?deep=false";
 		await fetch(this.host + "/search/" + searchTerm + queryString, { method: 'POST' })
-		.then((data) => {
-			return data.json();
-		})
-		.then((json) => {
-			callback(json);
-		});
+			.then((data) => {
+				return data.json();
+			})
+			.then((json) => {
+				callback(json);
+			});
 	}
 }
