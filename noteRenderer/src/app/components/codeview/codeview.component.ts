3a649cf90242e52873cd6143f3b18e9d98e1e5ac
David Peet <davidpeetp@gmail.com>
2021-01-19 13:27:29 -0500

    Updated driver code styling and fixed latex macro syntax

--- noteRenderer/src/app/components/codeview/codeview.component.ts
+++ noteRenderer/src/app/components/codeview/codeview.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, Input, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { FetchRenderEventBusService } from '@services/fetch-render-event-bus.service';
 import { FileAccessAPIService } from '@services/file-access-api.service';
 import { KatexOptions } from 'ngx-markdown';
- 
+
 @Component({
-  selector: 'app-codeview',
-  templateUrl: './codeview.component.html',
-  styleUrls: ['./codeview.component.sass']
+	selector: 'app-codeview',
+	templateUrl: './codeview.component.html',
+	styleUrls: ['./codeview.component.sass']
 })
 export class CodeviewComponent implements OnInit {
 	// View Displaying a note
 	@Input() willOverscroll: boolean = true;
-	@ViewChild('codeview') codeview; 
+	@ViewChild('codeview') codeview;
 	@ViewChild('content') content;
 	@ViewChild('scrollContent') scrollContent;
 	@ViewChild('displayText') displayText;
@@ -26,7 +26,7 @@ export class CodeviewComponent implements OnInit {
 			"\\paren": "\\left({#1}\\right)",
 			"\\brac": "\\left[{#1}\\right]",
 			"\\brace": "\\lbrace{#1}\\rbrace",
-			"\\angle": "\\langle{#1\\rangle",
+			"\\angle": "\\langle{#1}\\rangle",
 			"\\group": "\\lgroup{#1}\\rgroup",
 			"\\mn": "\\mathnormal{#1}",
 			"\\reg": "\\text{\\textdollar}{#1}",
@@ -35,16 +35,14 @@ export class CodeviewComponent implements OnInit {
 			"\\Iff": "\\Longleftrightarrow",
 		}
 	}
-	
+
 	constructor(private eventBus: FetchRenderEventBusService, private fileAPI: FileAccessAPIService) { }
 
-	ngOnInit(): void 
-	{
+	ngOnInit(): void {
 		this.eventBus.subscribeCodeEvent(this.setCode.bind(this));
 	}
 
-	setCode(): void 
-	{
+	setCode(): void {
 		// Fetch the content Required, then set it here
 		this.renderContent = this.fileAPI.getCurrentFile();
 		this.codeview.nativeElement.scrollTop = 0;
@@ -54,9 +52,8 @@ export class CodeviewComponent implements OnInit {
 	// TODO: Use Angular Observables
 	// Problem is that these values are being fetched before the render content is updated, so the overscroll is always one late
 	// This should really be implemented using Angular Observables
-	setOverscroll(): void
-	{
-		if(!this.willOverscroll) return;
+	setOverscroll(): void {
+		if (!this.willOverscroll) return;
 
 		let editorScreenHeight = this.codeview.nativeElement.offsetHeight;
 		let contentScrollHeight = this.scrollContent.nativeElement.scrollHeight;
